6348301850a07de64748701416f42c783574be0c
Garrett59100 <garrett59100@gmail.com>
2024-12-02 00:57:57 -0500

    day2 part1

---
+++ day2/main.py
@@ -0,0 +1,60 @@
+def intify_sequence(sequence):
+	new_sequence = []
+	for i in sequence:
+		new_sequence.append(int(i))
+	return new_sequence
+
+def is_all_decreasing(sequence):
+	last_item = -1
+	for i in sequence:
+		if last_item == -1:
+			last_item = i
+			continue
+
+		if last_item <= i:
+			return False
+		last_item = i
+
+	return True
+
+def is_all_increasing(sequence):
+	last_item = -1
+	for i in sequence:
+		if last_item == -1:
+			last_item = i
+			continue
+
+		if last_item >= i:
+			return False
+		last_item = i
+
+	return True
+
+def gap_is_ok(sequence):
+	last_item = -1
+	for i in sequence:
+		if last_item == -1:
+			last_item = i
+			continue
+
+		distance = abs(last_item - i)
+
+		if distance < 1 or distance > 3:
+			return False
+
+		last_item = i
+	return True
+
+def day1():
+	safe_reports = 0
+	with open("./input.txt") as f:
+		for row in f:
+			sequence = intify_sequence(row.split(" "))
+
+			if (is_all_decreasing(sequence) or is_all_increasing(sequence)) and gap_is_ok(sequence):
+				safe_reports += 1
+
+	print(safe_reports)
+
+if __name__ == "__main__":
+	day1()
