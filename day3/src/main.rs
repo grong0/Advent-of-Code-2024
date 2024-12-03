eb31f2fba60dcedc41045b055ea0b51f8953c38e
Garrett59100 <garrett59100@gmail.com>
2024-12-03 00:55:35 -0500

    day3 part2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -5,7 +5,7 @@ fn input() -> String {
 	return fs::read_to_string("./input.txt").unwrap();
 }
 
-fn parseMul(mul: &str) -> i32 {
+fn parse_mul(mul: &str) -> i32 {
 	let spl: Vec<&str> = mul.split(",").collect();
 	let re = Regex::new("\\d+").unwrap();
 	
@@ -32,12 +32,45 @@ fn part1() {
 	
 	let mut tot = 0;
 	for mul in muls {
-		tot += parseMul(mul);
+		tot += parse_mul(mul);
+	}
+	
+	println!("{}", tot);
+}
+
+fn part2() {
+	
+	let input = input();
+	let lines = input.split("\n");
+	let re = Regex::new("mul\\(\\d+,\\d+\\)|do\\(\\)|don't\\(\\)").unwrap();
+	let mut funcs: Vec<&str> = vec![];
+	for line in lines {
+		let values: Vec<&str> = re.captures_iter(line).map(|x| {
+			return x.get(0).unwrap().as_str();
+		}).collect();
+		funcs.extend(values);
+	}
+	
+	let mut tot = 0;
+	let mut current_mod = "do()";
+	for func in funcs {
+		println!("{}", func);
+		if !func.contains("mul") {
+			current_mod = func;
+			continue;
+		}
+		
+		if current_mod.eq("don't()") {
+			// current_mod = "do()";
+			continue;
+		}
+		
+		tot += parse_mul(func);
 	}
 	
 	println!("{}", tot);
 }
 
 fn main() {
-    part1();
+    part2();
 }
