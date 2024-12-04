10560c02c93551d6cf3deb9f7ad970f614c90390
Garrett59100 <garrett59100@gmail.com>
2024-12-04 03:02:56 -0500

    day4 part2

--- day4/main.go
+++ day4/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	// "math"
 	"os"
 	"regexp"
 	"strings"
@@ -14,46 +12,7 @@ func check(er error) {
 	}
 }
 
-func parseDiagnallyLeft(file []byte) {
-	array := strings.Split(string(file), "\n")
-	array = array[:len(array)-1]
-
-	// for i := 0; i < len(array); i++ {
-	// 	for z := i; z >= 0; z-- {
-	// 		fmt.Print(string(array[z][i-z]))
-	// 	}
-	// 	fmt.Println("")
-	// }
-	for i := len(array) - 1; i > 0; i-- {
-		// fmt.Println(i)
-		// fmt.Print(" ")
-		for z := i; z < len(array[i])-i; z++ {
-			// fmt.Print(z)
-			fmt.Print(string(array[z][len(array[i])-z-1]))
-			// fmt.Println(z, len(array[i])-z-1)
-			// fmt.Print(" ")
-		}
-		fmt.Println("")
-	}
-	// for i := 0; i < len(array); i++ {
-	// 	for z := 0; z < len(array[0]); z++ {
-	// 		fmt.Print(string(array[i+z][z]))
-	// 	}
-	// 	fmt.Println("")
-	// }
-}
-
-// func getRotate(file []byte) string {
-// 	str := string(file)
-
-// 	strings.Split(str, "\n")
-
-// 	newArr := new([]string)
-// }
-
-func main() {
-	fmt.Println("starting...")
-
+func day1() {
 	file, err := os.ReadFile("./input.txt")
 	check(err)
 
@@ -75,12 +34,12 @@ func main() {
 	horFor := len(rx.FindAllString(string(file), -1))
 	horBac := len(rs.FindAllString(string(file), -1))
 	tot += horFor + horBac
-	println("hor\t", horFor + horBac)
+	println("hor\t", horFor+horBac)
 	// vertical
 	verFor := len(rx.FindAllString(modFile, -1))
 	verBack := len(rs.FindAllString(modFile, -1))
 	tot += verFor + verBack
-	println("ver\t", verFor + verBack)
+	println("ver\t", verFor+verBack)
 	// diagnal
 	nwCount := 0
 	neCount := 0
@@ -90,22 +49,22 @@ func main() {
 		for z := 0; z < len(spl[i]); z++ {
 			if string(spl[i][z]) == "X" {
 				// north west
-				if i-3 >= 0 && z-3 >= 0 && (string(spl[i][z]) + string(spl[i-1][z-1]) + string(spl[i-2][z-2]) + string(spl[i-3][z-3])) == "XMAS" {
+				if i-3 >= 0 && z-3 >= 0 && (string(spl[i][z])+string(spl[i-1][z-1])+string(spl[i-2][z-2])+string(spl[i-3][z-3])) == "XMAS" {
 					tot += 1
 					nwCount += 1
 				}
 				// north east
-				if i-3 >= 0 && z+3 < len(spl[0]) && (string(spl[i][z]) + string(spl[i-1][z+1]) + string(spl[i-2][z+2]) + string(spl[i-3][z+3])) == "XMAS" {
+				if i-3 >= 0 && z+3 < len(spl[0]) && (string(spl[i][z])+string(spl[i-1][z+1])+string(spl[i-2][z+2])+string(spl[i-3][z+3])) == "XMAS" {
 					tot += 1
 					neCount += 1
 				}
 				// south west
-				if i+3 < len(spl) && z-3 >= 0 && (string(spl[i][z]) + string(spl[i+1][z-1]) + string(spl[i+2][z-2]) + string(spl[i+3][z-3])) == "XMAS" {
+				if i+3 < len(spl) && z-3 >= 0 && (string(spl[i][z])+string(spl[i+1][z-1])+string(spl[i+2][z-2])+string(spl[i+3][z-3])) == "XMAS" {
 					tot += 1
 					swCount += 1
 				}
 				// south east
-				if i+3 < len(spl) && z+3 < len(spl[0]) && (string(spl[i][z]) + string(spl[i+1][z+1]) + string(spl[i+2][z+2]) + string(spl[i+3][z+3])) == "XMAS" {
+				if i+3 < len(spl) && z+3 < len(spl[0]) && (string(spl[i][z])+string(spl[i+1][z+1])+string(spl[i+2][z+2])+string(spl[i+3][z+3])) == "XMAS" {
 					tot += 1
 					seCount += 1
 				}
@@ -116,5 +75,37 @@ func main() {
 	println("ne\t", neCount)
 	println("sw\t", swCount)
 	println("se\t", seCount)
-	fmt.Println("tot\t", tot)
+	println("tot\t", tot)
+}
+
+func day2() {
+	file, err := os.ReadFile("./input.txt")
+	check(err)
+	spl := strings.Split(string(file), "\n")
+	spl = spl[:len(spl)-1]
+
+	tot := 0
+	for i := 0; i < len(spl); i++ {
+		for z := 0; z < len(spl[i]); z++ {
+			if string(spl[i][z]) == "A" && (i-1 >= 0 && i+1 < len(spl) && z-1 >= 0 && z+1 < len(spl[0])) {
+				nw := string(spl[i-1][z-1])
+				ne := string(spl[i-1][z+1])
+				sw := string(spl[i+1][z-1])
+				se := string(spl[i+1][z+1])
+				comb := nw + ne + sw + se
+				
+				if comb == "MSMS" || comb == "MMSS" || comb == "SMSM" || comb == "SSMM" {
+					tot += 1
+				}
+			}
+		}
+	}
+	println("tot\t", tot)
+}
+
+func main() {
+	println("starting...")
+
+	// day1()
+	day2()
 }
