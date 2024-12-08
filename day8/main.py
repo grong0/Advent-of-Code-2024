f071a83bf4b0fd9be6d4a49ea20045f1bb83996f
Garrett59100 <garrett59100@gmail.com>
2024-12-08 00:55:05 -0500

    day8 part2

--- day8/main.py
+++ day8/main.py
@@ -14,9 +14,6 @@ def main():
             grid.append(list(row.strip("\n")))
             grid2.append(list(row.strip("\n")))
 
-    # for row in grid:
-    #     print("".join(row))
-
     frequencies = set()
     for row in grid:
         for col in row:
@@ -26,24 +23,31 @@ def main():
     antinode_locations = set()
     for frequency in frequencies:
         locations = get_locations(grid, frequency)
+        antinode_locations.update(locations)
         for loc_1 in locations:
             for loc_2 in locations:
                 if loc_1[0] == loc_2[0] and loc_1[1] == loc_2[1]:
                     continue
                 row_distance = loc_1[0] - loc_2[0]
                 col_distance = loc_1[1] - loc_2[1]
-                anti_loc_1 = (loc_2[0] - row_distance, loc_2[1] - col_distance)
-                anti_loc_2 = (loc_1[0] + row_distance, loc_1[1] + col_distance)
+                # from loc_1
+                anti_loc = (loc_1[0] + row_distance, loc_1[1] + col_distance)
+                while anti_loc[0] >= 0 and anti_loc[0] < len(grid) and anti_loc[1] >= 0 and anti_loc[1] < len(grid[0]):
+                    antinode_locations.add(anti_loc)
+                    grid2[anti_loc[0]][anti_loc[1]] = "#"
+                    anti_loc = (anti_loc[0] + row_distance, anti_loc[1] + col_distance)
+                # from loc_2
+                anti_loc = (loc_2[0] - row_distance, loc_2[1] - col_distance)
+                while anti_loc[0] >= 0 and anti_loc[0] < len(grid) and anti_loc[1] >= 0 and anti_loc[1] < len(grid[0]):
+                    antinode_locations.add(anti_loc)
+                    grid2[anti_loc[0]][anti_loc[1]] = "#"
+                    anti_loc = (anti_loc[0] - row_distance, anti_loc[1] - col_distance)
 
-                if anti_loc_1[0] >= 0 and anti_loc_1[0] < len(grid) and anti_loc_1[1] >= 0 and anti_loc_1[1] < len(grid[0]):
-                    antinode_locations.add(anti_loc_1)
-                    # grid2[anti_loc_1[0]][anti_loc_1[1]] = "#"
-                if anti_loc_2[0] >= 0 and anti_loc_2[0] < len(grid) and anti_loc_2[1] >= 0 and anti_loc_2[1] < len(grid[0]):
-                    antinode_locations.add(anti_loc_2)
-                    # grid2[anti_loc_2[0]][anti_loc_2[1]] = "#"
+    for row in grid:
+        print("".join(row))
+    for row in grid2:
+        print("".join(row))
 
-    # for row in grid2:
-    #     print("".join(row))
     print(len(antinode_locations))
 
 
